c0d31f52787ab87f641c4bfb793a7e9d1c4f1ec4
Albazc <albazcano@gmail.com>
2022-02-22 20:52:05 +0100

    modified index.html, main.js deleted image

--- main.js
+++ main.js
@@ -2,7 +2,33 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image:"/assets/vmSocks-green.jpg.png",
-        inventory: 8
+        image:"/assets/vmSocks-green.png",
+        inStock: true,
+        details: ["80% Cotton", "20% Poliestyer", "Gender-neutral"],
+        variants:[
+            {
+
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: "./assets/vmSocks-green.png"
+            },
+            {
+
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: "./assets/vmSocks-blue.jpg"
+            },
+
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function() {
+            this.cart +=1
+        },
+
+        updateProduct: function(variantImage) {
+            this.image = variantImage
+        }
     }
 })
